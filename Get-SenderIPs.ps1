cd520f567d1f630d18bfc4bf694a38cb78056016
erratic-pAtterns <erratic-pAtterns@users.noreply.github.com>
2017-05-24 14:12:26 +0200

    Update Get-SenderIPs.ps1

--- Get-SenderIPs.ps1
+++ Get-SenderIPs.ps1
@@ -1,37 +1,37 @@
-﻿function Get-SenderIPs {
-
-  param(
-    [Parameter(Mandatory=$true)][string[]]$domains,
-    [Parameter(Mandatory=$false)][int]$c
-  )
-  
-  # infinite loop protection - the 'redirect' modifier and 'include' mechanism may create infinite loops
-  if ($c -ge 11) {break}
-
-  $ipList = @();
-
-  foreach ($domain in $domains) {
-
-    $txtQuery = Resolve-DnsName -Type TXT -Name $domain | where {$_.Strings -like "*spf1*"}
-    # skip domain if no SPF record found
-    if (!$txtQuery) {continue};
+function Get-SenderIPs {
     
-    $spfRecord = $txtQuery.Strings.Split(" ");
+    param(
+        [Parameter(Mandatory=$true)][string[]]$domains,
+        [Parameter(Mandatory=$false)][int]$c
+    )
 
-    foreach ($spfTerm in $spfRecord) {
+    # infinite loop protection - the 'redirect' modifier and 'include' mechanism may create infinite loops
+    if ($c -ge 11) {break}
 
-      switch -wildcard ($spfTerm) 
-        {
-          "mx"         {$ipList += Resolve-DnsName -Type MX -Name $domain | foreach {$_.IPAddress}; break}
-          "mx:*"       {$ipList += Resolve-DnsName -Type MX -Name $spfTerm.Substring(3) | foreach {$_.IPAddress}; break}
-          "a"          {$ipList += Resolve-DnsName -Type A -Name $domain | foreach {$_.IPAddress}; break}
-          "a:*"        {$ipList += Resolve-DnsName -Type A -Name $spfTerm.Substring(2) | foreach {$_.IPAddress}; break}
-          "ip4:*"      {$ipList += $spfTerm.Substring(4); break}
-          "include:*"  {$ipList += get-SenderIPs $spfTerm.Substring(8) ($c + 1); break}
-          "redirect=*" {$ipList += get-SenderIPs $spfTerm.Substring(9) ($c + 1); break}
+    $ipList = @()
+
+    foreach ($domain in $domains) {
+
+        $txtQuery = Resolve-DnsName -Type TXT -Name $domain | where {$_.Strings -like "*spf1*"}
+        # skip domain if no SPF record found
+        if (!$txtQuery) {continue}
+        
+        $spfRecord = $txtQuery.Strings.Split(" ")
+
+        foreach ($spfTerm in $spfRecord) {
+
+            switch -wildcard ($spfTerm) 
+                {
+                    "mx"         {$ipList += Resolve-DnsName -Type MX -Name $domain | foreach {$_.IPAddress}; break}
+                    "mx:*"       {$ipList += Resolve-DnsName -Type MX -Name $spfTerm.Substring(3) | foreach {$_.IPAddress}; break}
+                    "a"          {$ipList += Resolve-DnsName -Type A -Name $domain | foreach {$_.IPAddress}; break}
+                    "a:*"        {$ipList += Resolve-DnsName -Type A -Name $spfTerm.Substring(2) | foreach {$_.IPAddress}; break}
+                    "ip4:*"      {$ipList += $spfTerm.Substring(4); break}
+                    "include:*"  {$ipList += get-SenderIPs $spfTerm.Substring(8) ($c + 1); break}
+                    "redirect=*" {$ipList += get-SenderIPs $spfTerm.Substring(9) ($c + 1); break}
+                }
         }
     }
-  }
-  # remove duplicate IPs and return the IP list
-  Write-Output $ipList | select -unique;
+    # remove duplicate IPs and return the IP list
+    Write-Output $ipList | select -unique
 }
