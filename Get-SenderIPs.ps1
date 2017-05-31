b85711f9e20ef670c631ec74228bae9089af10f5
erratic-pAtterns <erratic-pAtterns@users.noreply.github.com>
2017-05-31 13:56:26 +0200

    domain name validation + DNS error catch added
    + some additional comments

--- Get-SenderIPs.ps1
+++ Get-SenderIPs.ps1
@@ -15,9 +15,27 @@ function Get-SenderIPs {
 
     foreach ($domain in $domains) {
 
-        $txtQuery = Resolve-DnsName -Type TXT -Name $domain | where {$_.Strings -like "*spf1*"}
+        # validate domain name
+        if ($domain -notmatch "^[\w]+([\-\.]{1}[\w]+)*\.[a-z]+$") {
+            Write-Host "ERROR: `"$domain`" is not a valid domain name. Use <domain>.<tld>" -foreground "red"
+            exit
+        }
+        
+        # retrieve SPF record
+        try {
+            $txtQuery = Resolve-DnsName -Type TXT -Name $domain -ErrorAction Stop| where {$_.Strings -like "*spf1*"}
+        } catch [System.ComponentModel.Win32Exception] {
+            if ($_.Exception.Message -like "*name does not exist") {
+                Write-Host "The domain `"$domain`" does not exist!`n" -foreground "cyan"
+                continue
+            }
+        }
+        
         # skip domain if no SPF record found
-        if (!$txtQuery) {continue}
+        if (!$txtQuery) {
+            Write-Host "No SPF record found for `"$domain`"`n" -foreground "cyan"
+            continue
+        }
         
         $spfRecord = $txtQuery.Strings.Split(" ")
 
@@ -30,17 +48,17 @@ function Get-SenderIPs {
                     "a"          {$ipList += Resolve-DnsName -Type A -Name $domain | foreach {$_.IPAddress}; break}
                     "a:*"        {$ipList += Resolve-DnsName -Type A -Name $spfTerm.Substring(2) | foreach {$_.IPAddress}; break}
                     "ip4:*"      {$ipList += $spfTerm.Substring(4); break}
-                    "include:*"  {$ipList += Get-SenderIPs $spfTerm.Substring(8) ($cd + 1) -d:$d; break}
-                    "redirect=*" {$ipList += Get-SenderIPs $spfTerm.Substring(9) ($cd + 1) -d:$d; break}
+                    "include:*"  {$ipList += Get-SenderIPs -domains $spfTerm.Substring(8) -cd ($cd + 1) -d:$d; break}
+                    "redirect=*" {$ipList += Get-SenderIPs -domains $spfTerm.Substring(9) -cd ($cd + 1) -d:$d; break}
                 }
         }
-        # Switch: concatenate $domain to the end of the IP addresses
+        # Switch: add $domain to the end of the IP addresses
         if ($d -And $cd -eq 0) {
             # TAB delimited
             $ipList = $ipList -replace "[\d\.\/]+$", "$&`t$domain"
         }
     }
-
+    
     # remove duplicate IPs and return the IP list
     Write-Output $ipList | select -unique
 }
